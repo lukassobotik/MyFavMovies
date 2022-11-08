5624bd4b48031a42a6855cc70c30512f3b1a34a9
PuckyEU <osmset@gmail.com>
2022-11-08 19:08:27 +0100

    Sliders are moving by the correct amount of items at a time, carousel scrolling is infinite, when rescaling the window the item on left stays at the correct place

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,17 +2,15 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import BrowseMovieCard from "./BrowseMovieCard";
 import {HiChevronLeft, HiChevronRight} from "react-icons/hi"
-import {useHistory} from "react-router-dom";
 import {screenSizeGroups} from "./Constants";
 
-const scrollAmountPerClick = 6;
-const movieItemWidth = 300;
-
-let pos = 1;
 export default function Row({title, fetchURL, rowId}) {
     const [allMovies, setAllMovies] = useState([]);
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    let scrollAmountPerClick = 3;
+    let movieItemWidth = 300;
+    let firstVisibleItemPosition = 0;
 
     useEffect(() => {
         axios.get(fetchURL).then((response) => {
@@ -25,22 +23,37 @@ export default function Row({title, fetchURL, rowId}) {
         })
     }, [fetchURL]);
 
-    let index = 0;
-
     window.addEventListener('resize', (() => {
-        if (window.innerWidth >= screenSizeGroups.sixItems) {
-            document.querySelectorAll(".movie_card_item").forEach(el => el.style.width = "16%");
-        } else if (window.innerWidth >= screenSizeGroups.fiveItems && window.innerWidth <= screenSizeGroups.sixItems) {
-            document.querySelectorAll(".movie_card_item").forEach(el => el.style.width = "19%");
-        } else if (window.innerWidth >= screenSizeGroups.fourItems && window.innerWidth <= screenSizeGroups.fiveItems) {
-            document.querySelectorAll(".movie_card_item").forEach(el => el.style.width = "24%");
-        } else if (window.innerWidth >= screenSizeGroups.threeItems && window.innerWidth <= screenSizeGroups.fourItems) {
-            document.querySelectorAll(".movie_card_item").forEach(el => el.style.width = "31.5%");
-        } else if (window.innerWidth <= screenSizeGroups.threeItems) {
-            document.querySelectorAll(".movie_card_item").forEach(el => el.style.width = "46%");
-        }
+        let element = document.getElementById("slider" + rowId);
+        movieItemWidth = document.getElementById("itemId1-" + rowId).clientWidth;
+        element.style.left = (-firstVisibleItemPosition * movieItemWidth) + "px";
+
+        setItemScaling();
     }))
 
+    function setItemScaling() {
+        if (window.innerWidth >= screenSizeGroups.sixItems) {
+            document.querySelectorAll(".movie_card_item").forEach(el => el.style.width = "16.666%");
+            document.getElementById("root").setAttribute("itemsonscreen", "6");
+            scrollAmountPerClick = 6;
+        } else if (window.innerWidth >= screenSizeGroups.fiveItems && window.innerWidth <= screenSizeGroups.sixItems) {
+            document.querySelectorAll(".movie_card_item").forEach(el => el.style.width = "20%");
+            document.getElementById("root").setAttribute("itemsonscreen", "5");
+            scrollAmountPerClick = 5;
+        } else if (window.innerWidth >= screenSizeGroups.fourItems && window.innerWidth <= screenSizeGroups.fiveItems) {
+            document.querySelectorAll(".movie_card_item").forEach(el => el.style.width = "25%");
+            document.getElementById("root").setAttribute("itemsonscreen", "4");
+            scrollAmountPerClick = 4;
+        } else if (window.innerWidth >= screenSizeGroups.threeItems && window.innerWidth <= screenSizeGroups.fourItems) {
+            document.querySelectorAll(".movie_card_item").forEach(el => el.style.width = "33.333%");
+            document.getElementById("root").setAttribute("itemsonscreen", "3");
+            scrollAmountPerClick = 3;
+        } else if (window.innerWidth <= screenSizeGroups.threeItems) {
+            document.querySelectorAll(".movie_card_item").forEach(el => el.style.width = "50%");
+            document.getElementById("root").setAttribute("itemsonscreen", "2");
+            scrollAmountPerClick = 2;
+        }
+    }
 
     const setLeftOrigin = () => {
         let numvalue = parseInt(document.getElementById("slider" + rowId)?.getAttribute('numvalue'));
@@ -55,61 +68,60 @@ export default function Row({title, fetchURL, rowId}) {
     }
 
     const setAnimation = (direction) => {
-        let elements = document.getElementById("slider" + rowId).querySelectorAll(".row_item");
-        for (let i = 0; i < allMovies.length; i++) {
-            let left = (parseInt(elements[i].style.left, 10));
-            let width = (document.getElementById("root").scrollWidth) - (movieItemWidth * scrollAmountPerClick);
-            if (direction === "right" && left > (-width)) {
-                elements[i].style.left = left + (-movieItemWidth) + "px";
-            } else if (direction === "left" && left < 0) {
-                elements[i].style.left = left + movieItemWidth + "px";
-            } else if (direction !== "left" && direction !== "right"){
-                console.error("wrong direction attribute");
-            }
+        if (direction !== "left" && direction !== "right") {
+            console.error("wrong direction attribute");
+            return;
+        }
+        let element = document.getElementById("slider" + rowId);
+        movieItemWidth = document.getElementById("itemId1-1").clientWidth;
+
+        console.log("pos by division: ", ((-parseInt(element.style.left.substring(0, element.style.left.length - 2)) / movieItemWidth) + 1))
+        firstVisibleItemPosition = (-parseInt(element.style.left.substring(0, element.style.left.length - 2)) / movieItemWidth) + 1;
+        if (direction === "right" && firstVisibleItemPosition + (scrollAmountPerClick - 1) >= allMovies.length) return;
+        if (direction === "left" && firstVisibleItemPosition - 1 <= 0) return;
+
+        let left = (parseInt(element.style.left, 10));
+        if (direction === "right") {
+            element.style.left = left + (-movieItemWidth) + "px";
+        } else if (direction === "left" && left < 0) {
+            element.style.left = left + movieItemWidth + "px";
         }
     }
 
+    let triggerAmount = 0;
     const left = () => {
-        resetOrigin(pos);
+        resetOrigin(firstVisibleItemPosition);
         for (let i = 0; i <= scrollAmountPerClick; i++) {
-            pos = parseInt(document.getElementById("slider" + rowId)?.getAttribute('numvalue'));
-            let element = document.getElementById("slider" + rowId);
-
-            if (pos - 1 > 0) pos--;
-
             setAnimation("left");
-
-            element.setAttribute("numvalue", pos.toString());
         }
-
         setLeftOrigin();
+
+        triggerAmount = 0;
     }
     const right = () => {
+        let element = document.getElementById("slider" + rowId);
         for (let i = 0; i < scrollAmountPerClick; i++) {
-            let element = document.getElementById("slider" + rowId);
-            pos = parseInt(document.getElementById("slider" + rowId)?.getAttribute('numvalue'));
-
-            if (pos + 1 < allMovies.length) pos++;
-
             setAnimation("right");
-
-            element.setAttribute("numvalue", pos.toString());
         }
-
         setLeftOrigin();
-    }
 
-    useHistory().listen(() => {
-        pos = 1;
-    })
+        if (firstVisibleItemPosition !== (allMovies.length - (scrollAmountPerClick - 1))) return;
+        if (triggerAmount === 1) {
+            triggerAmount = 0;
+            element.style.left = "0";
+        } else triggerAmount++;
+    }
+    let index = 0;
 
     return (
         !isLoading && <div className="">
             <h2 className='ml-[50px] text-white font-bold md:text-xl p-4 text-left'> {title} </h2>
             <div id={"row:" + rowId} className="carousel_row relative flex whitespace-nowrap items-center group">
                 <div id={'slider' + rowId}
-                     className="slider ml-[50px] w-full h-full relative"
-                     numvalue={1}>
+                     className="slider ml-[50px] mr-[50px] w-full h-full relative"
+                     style={{left: 0}}
+                     numvalue={1}
+                     onLoad={setItemScaling}>
                     {movies?.map((item, id) => {
                         index++;
                         return (<BrowseMovieCard key={id} item={item} index={index} rowId={rowId} type={item.media_type ? item.media_type : "movie"}/>)
