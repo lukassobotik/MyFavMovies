ea07eb979d4ad4cef8c445b7f7559afc5104aa2f
PuckyEU <osmset@gmail.com>
2022-12-03 11:29:51 +0100

    Movie Collection Link

--- src/components/Movie Route/Movie.js
+++ src/components/Movie Route/Movie.js
@@ -342,6 +342,17 @@ export default function Movie() {
                         ))}
                     </div>
                 </div>
+                {item.belongs_to_collection ?
+                    <div id="franchise_ribbon" className="w-full h-full flex_center border-b-2 border-[#FFFFFF] whitespace-nowrap relative">
+                        <div className="img_bg w-full h-full">
+                            <img src={`https://image.tmdb.org/t/p/original/${item.belongs_to_collection.backdrop_path}`} alt={""} className="w-full h-full"/>
+                        </div>
+                        <div className="p-5 absolute whitespace-pre-wrap w-full h-full flex_center">
+                            <img src={`https://image.tmdb.org/t/p/original/${item.belongs_to_collection.poster_path}`} alt={""} className="h-full aspect-auto border-2 border-[#FFFFFF] rounded-3xl"/>
+                            <Link to={`/collection/${item.belongs_to_collection.id}/`}><div className="font-bold text-[4vw] ml-5">Belongs to the {item.belongs_to_collection.name}</div></Link>
+                        </div>
+                    </div>
+                    : null}
             </div>
         </Layout>
     )
