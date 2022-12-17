83a93f9e46a1e8ec3e2ebaa169de67b9ce21d446
PuckyEU <osmset@gmail.com>
2022-12-17 11:51:06 +0100

    "Because you liked..." Recommendations

--- src/LoadData.js
+++ src/LoadData.js
@@ -1,9 +1,9 @@
-import {collection, getDocs} from "firebase/firestore";
+import {collection, getDocs, query, orderBy, limit} from "firebase/firestore";
 import {auth, db} from "./firebase";
 
 export default async function LoadSettingsData() {
-    const ratingSnapshot = await getDocs(collection(db, "users", auth.currentUser.uid.toString(), "settings"));
-    ratingSnapshot.forEach((doc) => {
+    const settingsSnapshot = await getDocs(collection(db, "users", auth.currentUser.uid.toString(), "settings"));
+    settingsSnapshot.forEach((doc) => {
         if (doc.data().loc_iso !== undefined && doc.data().loc_iso !== null) {
             document.getElementById("root").setAttribute("locvalue", doc.data().loc_iso);
         }
@@ -11,4 +11,16 @@ export default async function LoadSettingsData() {
             document.getElementById("root").setAttribute("langvalue", doc.data().lang_iso);
         }
     });
+}
+
+export async function LoadRatings() {
+    const ratingQuery = query(collection(db, "users", auth.currentUser.uid.toString(), "ratings"), orderBy("rating", "desc"), limit(9));
+    const ratingsSnapshot = await getDocs(ratingQuery);
+    let ratings = [];
+    ratingsSnapshot.forEach((doc) => {
+        if (doc.data().rating > 6) {
+            ratings.push(doc.data());
+        }
+    });
+    return ratings.sort(() => .5 - Math.random()).slice(0,3);
 }
--- src/components/Browse Route/Browse.js
+++ src/components/Browse Route/Browse.js
@@ -3,12 +3,14 @@ import requests from "../../Constants";
 import Row from "./Row";
 import {useEffect} from "react";
 import {auth} from "../../firebase";
-import LoadSettingsData from "../../LoadData";
+import LoadSettingsData, {LoadRatings} from "../../LoadData";
 import {useState} from "react";
 import './Browse.css';
 
 export default function Browse() {
     const [isLoading, setIsLoading] = useState(true);
+    const [ratedMovies, setRatedMovies] = useState([]);
+    const [hasLoaded, setHasLoaded] = useState(false);
 
     useEffect(() => {
         auth.onAuthStateChanged(user => {
@@ -21,6 +23,10 @@ export default function Browse() {
     }, [isLoading]);
 
     async function loadData() {
+        if (!hasLoaded) {
+            setRatedMovies(await LoadRatings());
+            setHasLoaded(true);
+        }
         await LoadSettingsData();
         setIsLoading(false);
     }
@@ -28,10 +34,13 @@ export default function Browse() {
     return (
         !isLoading && <Layout>
             <div className="browse_container">
-                <Row rowId="1" title='Now Playing' fetchURL={requests.now_playing}/>
+                {ratedMovies[0] ? <Row rowId="1" title={`Because you liked ${ratedMovies[0].name}`} fetchURL={`https://api.themoviedb.org/3/movie/${ratedMovies[0].movieId}/similar?api_key=${requests.key}&language=${document.getElementById("root")?.getAttribute('langvalue')}&page=1`}/> : null}
                 <Row rowId="2" title='Upcoming' fetchURL={requests.upcoming}/>
-                <Row rowId="3" title='Popular' fetchURL={requests.popular}/>
-                <Row rowId="4" title='Trending this Week' fetchURL={requests.trending_this_week}/>
+                {ratedMovies[1] ? <Row rowId="3" title={`Because you liked ${ratedMovies[1].name}`} fetchURL={`https://api.themoviedb.org/3/movie/${ratedMovies[1].movieId}/similar?api_key=${requests.key}&language=${document.getElementById("root")?.getAttribute('langvalue')}&page=1`}/> : null}
+                <Row rowId="4" title='Now Playing' fetchURL={requests.now_playing}/>
+                {ratedMovies[2] ? <Row rowId="5" title={`Because you liked ${ratedMovies[2].name}`} fetchURL={`https://api.themoviedb.org/3/movie/${ratedMovies[2].movieId}/similar?api_key=${requests.key}&language=${document.getElementById("root")?.getAttribute('langvalue')}&page=1`}/> : null}
+                <Row rowId="6" title='Popular' fetchURL={requests.popular}/>
+                <Row rowId="7" title='Trending this Week' fetchURL={requests.trending_this_week}/>
             </div>
         </Layout>
     );
